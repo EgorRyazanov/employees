a098f700aa35a7d61c7638ece13445f8db82662f
Egor Ryazanov <egor.ryazanov@saritasa.com>
2024-01-07 21:39:52 +0500

    Add divider

--- src/features/emploeesScheme/components/Node/Node.tsx
+++ src/features/emploeesScheme/components/Node/Node.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Typography } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
@@ -15,6 +15,8 @@ import { NodeDetailsModal } from '../NodeDetailsModal/NodeDetailsModal';
 import { filtersSelectors } from '../../../../store/filters/selectors';
 import { NodeViews } from '../../../../models/nodeVIew';
 import { EmployeeViews } from '../../../../models/employeeViews';
+import { Structure } from '../../../../models/structure';
+import { getNodeTitle } from '../../../../utils/getNodeTitle';
 
 interface NodeComponentProps {
   node: NodeType;
@@ -68,86 +70,90 @@ const NodeComponent: FC<NodeComponentProps> = ({ node, space }) => {
   };
 
   return (
-    <Box sx={{ paddingLeft: `${space ?? 0}px` }}>
-      <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
-        {(node.employees.length > 0 || node.next.length > 0 || node.employers.length > 0) && (
-          <IconButton onClick={handleClick}>
-            {isActive ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        )}
-        <Box
-          onClick={() => handleMainNodeClick(node)}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '16px',
-            cursor: 'pointer',
-            marginLeft: node.employees.length > 0 || node.next.length > 0 || node.employers.length > 0 ? 0 : '50px',
-          }}>
-          <GroupIcon sx={{ color: '#A8A19A' }} />
-          <Box>
-            <Typography>{node.name}</Typography>
-            {node.vacancyCount !== 0 && paramsOptions?.nodeViews.includes(NodeViews.Vacancies) && (
-              <Typography sx={{ color: '#A8A19A' }}>{node.vacancyCount} вакансии</Typography>
-            )}
+    <>
+      <Box sx={{ paddingLeft: `${space ?? 0}px` }}>
+        <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+          {(node.employees.length > 0 || node.next.length > 0 || node.employers.length > 0) && (
+            <IconButton onClick={handleClick}>
+              {isActive ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          )}
+          <Box
+            onClick={() => handleMainNodeClick(node)}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: '16px',
+              cursor: 'pointer',
+              marginLeft: node.employees.length > 0 || node.next.length > 0 || node.employers.length > 0 ? 0 : '50px',
+            }}>
+            <GroupIcon sx={{ color: '#A8A19A' }} />
+            <Box>
+              <Typography>{node.name}</Typography>
+              {node.vacancyCount !== 0 && paramsOptions?.nodeViews.includes(NodeViews.Vacancies) && (
+                <Typography sx={{ color: '#A8A19A' }}>{node.vacancyCount} вакансии</Typography>
+              )}
+            </Box>
           </Box>
         </Box>
+        {isActive && (
+          <Box>
+            {node.next.map(nextNode => (
+              <Node key={nextNode.id} node={nextNode} space={16} />
+            ))}
+            {(shouldShowNode || paramsOptions?.nodeViews.includes(NodeViews.Employees)) && (
+              <>
+                <Typography>{getNodeTitle(node.structureEnum)}</Typography>
+                <Box
+                  sx={{ maxHeight: '280px', overflowY: 'auto', cursor: 'pointer' }}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                  {node.employers.map(
+                    person =>
+                      !person.isVacancy && (
+                        <Box
+                          onClick={() => handlePersonClick(person.id)}
+                          key={person.id}
+                          sx={{ padding: '8px 12px', backgroundColor: '#E8F5E9' }}>
+                          <Typography>{person.fullName}</Typography>
+                          {paramsOptions?.employeeViews.includes(EmployeeViews.Position) && (
+                            <Typography sx={{ color: '#A8A19A' }} variant="body2">
+                              {person.position}
+                            </Typography>
+                          )}
+                        </Box>
+                      ),
+                  )}
+                </Box>
+                <Box
+                  sx={{ paddingLeft: '16px', maxHeight: '280px', overflowY: 'auto', cursor: 'pointer' }}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                  {node.employees.map(
+                    person =>
+                      !person.isVacancy && (
+                        <Box onClick={() => handlePersonClick(person.id)} key={person.id} sx={{ padding: '8px 12px' }}>
+                          <Typography>{person.fullName}</Typography>
+                          {paramsOptions?.employeeViews.includes(EmployeeViews.Position) && (
+                            <Typography sx={{ color: '#A8A19A' }} variant="body2">
+                              {person.position}
+                            </Typography>
+                          )}
+                        </Box>
+                      ),
+                  )}
+                </Box>
+              </>
+            )}
+          </Box>
+        )}
+        {hasPersonModalOpen && <PersonModal isOpened={hasPersonModalOpen} toggleModal={handleDropPerson} />}
+        {hasMainNodeModalOpen && activeMainNode != null && (
+          <NodeDetailsModal isOpened={hasMainNodeModalOpen} node={activeMainNode} toggleModal={handleMainNodeDrop} />
+        )}
       </Box>
-      {isActive && (
-        <Box>
-          {node.next.map(nextNode => (
-            <Node key={nextNode.id} node={nextNode} space={16} />
-          ))}
-          {(shouldShowNode || paramsOptions?.nodeViews.includes(NodeViews.Employees)) && (
-            <>
-              <Box
-                sx={{ maxHeight: '280px', overflowY: 'auto', cursor: 'pointer' }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}>
-                {node.employers.map(
-                  person =>
-                    !person.isVacancy && (
-                      <Box
-                        onClick={() => handlePersonClick(person.id)}
-                        key={person.id}
-                        sx={{ padding: '8px 12px', backgroundColor: '#E8F5E9' }}>
-                        <Typography>{person.fullName}</Typography>
-                        {paramsOptions?.employeeViews.includes(EmployeeViews.Position) && (
-                          <Typography sx={{ color: '#A8A19A' }} variant="body2">
-                            {person.position}
-                          </Typography>
-                        )}
-                      </Box>
-                    ),
-                )}
-              </Box>
-              <Box
-                sx={{ paddingLeft: '16px', maxHeight: '280px', overflowY: 'auto', cursor: 'pointer' }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}>
-                {node.employees.map(
-                  person =>
-                    !person.isVacancy && (
-                      <Box onClick={() => handlePersonClick(person.id)} key={person.id} sx={{ padding: '8px 12px' }}>
-                        <Typography>{person.fullName}</Typography>
-                        {paramsOptions?.employeeViews.includes(EmployeeViews.Position) && (
-                          <Typography sx={{ color: '#A8A19A' }} variant="body2">
-                            {person.position}
-                          </Typography>
-                        )}
-                      </Box>
-                    ),
-                )}
-              </Box>
-            </>
-          )}
-        </Box>
-      )}
-      {hasPersonModalOpen && <PersonModal isOpened={hasPersonModalOpen} toggleModal={handleDropPerson} />}
-      {hasMainNodeModalOpen && activeMainNode != null && (
-        <NodeDetailsModal isOpened={hasMainNodeModalOpen} node={activeMainNode} toggleModal={handleMainNodeDrop} />
-      )}
-    </Box>
+      {node.structureEnum === Structure.Department && <Divider sx={{ position: 'absolute', left: 0, right: 0 }} />}
+    </>
   );
 };
 
